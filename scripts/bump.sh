f8507014a9e31df609153dfb39b4047acfa8fdbe
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2026-06-10 13:28:11 +0800

    fix: fix publish

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -5,12 +5,13 @@ set -e
 wd=$(pwd)
 retroarch_dir=$wd/modules/retroarch
 
-cd $retroarch_dir
+cd "$retroarch_dir"
 retroarch_version=$(git describe --tags $(git rev-list --tags --max-count=1) | sed 's/^v//')
 
 current_time=$(date "+%Y%m%d%H%M%S")
 
-cd $wd
+cd "$wd"
 version="$retroarch_version-$current_time"
-echo $version
+echo "$version"
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$version\"/" package.json
+git tag "v$version"
